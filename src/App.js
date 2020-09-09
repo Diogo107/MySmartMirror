d1c1748a2bacdbc6b1b027a317c28c4a2264d533
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-09-09 12:03:22 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div className="overall">
+			<div className="row">
+				<div className="box">Calendar</div>
+				{/* date */}
+				<div className="box">Clock</div>
+				<div className="box">weather</div>
+			</div>
+			<div className="row">Notifications</div>
+			<div className="row">
+				<div className="box">Stocks</div>
+				<div className="box">last emails</div>
+				<div className="box">News</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
